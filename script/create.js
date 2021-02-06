24398690269e3a2ff73a755476d7f19a7973565a
Yago Silva <silvayagot@gmail.com>
2021-02-06 13:59:08 -0300

    third commit

---
+++ script/create.js
@@ -0,0 +1,6 @@
+// verificar se o email da pessoa tem @ ou .com
+// verificar se o email da pessoa ja nn existe no local storage
+//verificar se a senha tem no minimo 6 digitos
+//gravar informaçoes no local storage
+//se tudo estiver certo quando ela clicar emsubmit ela vai para tela de login
+
---
+++ script/login.js
@@ -0,0 +1,2 @@
+//verificar se o email colocado existe no local storage
+//verificar se a senha bate com a do local storage
