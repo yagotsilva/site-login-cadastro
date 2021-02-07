e3a46e611d211ede87a4aeb9014dd057e979caf1
Yago Silva <silvayagot@gmail.com>
2021-02-07 18:15:38 -0300

    fourth commit

--- script/create.js
+++ script/create.js
@@ -1,6 +1,26 @@
-// verificar se o email da pessoa tem @ ou .com
-// verificar se o email da pessoa ja nn existe no local storage
-//verificar se a senha tem no minimo 6 digitos
-//gravar informaçoes no local storage
-//se tudo estiver certo quando ela clicar emsubmit ela vai para tela de login
+var register = function() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  localStorage.setItem('name', name)
+  localStorage.setItem('email', email)
+  localStorage.setItem('password', password)
+
+}
+document.onsubmit = register;
+
+
+var Form = document.getElementById('form')
+
+form.addEventListener('submit',function(event){
+  event.prenventDefault()
+
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+})
+
+
+
 
--- script/login.js
+++ script/login.js
@@ -1,2 +1,1 @@
-//verificar se o email colocado existe no local storage
-//verificar se a senha bate com a do local storage
+
