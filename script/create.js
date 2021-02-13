465348bc9d50ba79651c5c9ef27ef59b8e28d448
Yago Silva <silvayagot@gmail.com>
2021-02-13 13:39:01 -0300

    finish login and register

--- script/create.js
+++ script/create.js
@@ -1,34 +1,94 @@
-
-const registerClient = {
-
-  name() {
-    document.getElementById('name').value;
+//localstorage
+const Storage = {
+  get(){
+    return JSON.parse(localStorage.getItem("users:site-login")) ||
+    []
   },
-  email() {
-    document.getElementById('email').value;
-  },
-  
+
+  set(users) {
+    localStorage.setItem("users:site-login", JSON.stringify(users))
+  }
 }
- console.log(registerClient.name())
+
+// Users in the localstorage
+const Users = {
+  all: Storage.get(),
+  add(user){
+    Users.all.push(user)
+
+    Storage.set(Users.all)
+
+    App.reload()
+  },
+}
 
 const Form = {
   name: document.querySelector('input#name'),
   email: document.querySelector('input#email'),
+  password: document.querySelector('input#password'),
 
+  // get values of input and put in the users
   getValues() {
-    return{
+    return {
       name: Form.name.value,
-      email: form.email.value
+      email: Form.email.value,
+      password: Form.password.value
     }
   },
 
   validateFields() {
     const { name, email } = Form.getValues()
-    console.log(name)
+    if(name.trim() === "" ||
+    email.trim() === "" ){
+      throw new Error("please, complete the form")
+    }
+  },
+
+  // verification of email exist or don't in the sistem
+  userExist() {
+    const { email } = Form.getValues()
+    const user = Users.all.find(element => {
+      return element.email === email;
+    });
+    return user;
+  },
+  //clear the form
+  clearFields() {
+    Form.name.value = ""
+    Form.email.value = ""
+    Form.password.value = ""
   },
 
   submit(event) {
-    event.preventDefault()
-   
+    try{
+      event.preventDefault();
+      Form.validateFields();
+      
+      const user = Form.userExist();
+      
+      if (!user) {
+        Users.add(Form.getValues());
+        Form.clearFields();
+        window.location.href = "./login.html"
+      } else {
+        document.querySelector('input#email').classList.add('error')
+        document.querySelector('p#msg').classList.add('error')
+        // Form.clearFields();
+      }
+    } catch(error){
+      alert(error.message);
+    }
+    
   }
 }
+
+const App = {
+  init () {
+    Storage.set(Users.all)
+  },
+  reload() {
+    App.init()
+  }
+}
+
+App.init()
--- script/login.js
+++ script/login.js
@@ -1,10 +1,89 @@
-  form.addEventListener('submit',function(event){
-    event.preventDefault()
+const Storage = {
+  get(){
+    return JSON.parse(localStorage.getItem("users:site-login")) ||
+    []
+  },
+
+  set(users) {
+    localStorage.setItem("users:site-login", JSON.stringify(users))
+  }
+}
+
+// Users in the localstorage
+const Users = {
+  all: Storage.get(),
+  add(user){
+    Users.all.push(user)
+
+    Storage.set(Users.all)
+
+    App.reload()
+  },
+}
+
+const Form = {
+  email: document.querySelector('input.email'),
+  password: document.querySelector('input.password'),
+
+  getValues() {
+    return {
+      email: Form.email.value,
+      password: Form.password.value
+    }
+  },
+
+  validateFields() {
+    const { email, password } = Form.getValues()
+    if(email.trim() === "" ||
+    password.trim() === "" ){
+      throw new Error("please, complete the form")
+    }
+  },
+
+  userExist() {
+    const { email } = Form.getValues()
+    const user = Users.all.find(element => {
+      return element.email === email;
+    });
+    return user;
+  },
+
+  clearFields() {
+    Form.email.value = ""
+    Form.password.value = ""
+  },
+
+  submit(event) {
     
-  if(true){
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var email = localStorage.find(email)
+    try{
+      event.preventDefault();
+      Form.validateFields();
+      const { email } = Form.getValues()
+      
+      const user = Form.userExist();
+  
+      if (!user) {
+        document.querySelector('p.msg').classList.add('error')
+        Form.clearFields();
+
+      }else {
+        Form.clearFields();
+        window.location.href = "./initial-page.html"
+      }
+    }catch(error){
+      alert(error.message)
+    }
     
   }
-})
+}
+
+const App = {
+  init () {
+    Storage.set(Users.all)
+  },
+  reload() {
+    App.init()
+  }
+}
+
+App.init()
